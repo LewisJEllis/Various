b598f8a07f902b79c8c2b3ee4cd7db01bcb2598d
Lewis Ellis <ellisl@seas.upenn.edu>
2012-12-30 00:15:13 -0500

    gitignore test.py

--- ProjectEuler/test.py
+++
@@ -1,9 +0,0 @@
-import math
-#import timeit
-#print timeit.timeit('math.log10(2**10000)','import math')
-#print timeit.timeit('math.log10(2**100000)','import math')
-#print timeit.timeit('math.log10(2**1000000)','import math')
-
-for i, v in enumerate(xrange(2,5)):
-  print i
-  print v
