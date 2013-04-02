71b29e8ccffeb0af792729c0f9d1cf0c2eba0515
Lewis Ellis <ellisl@seas.upenn.edu>
2013-04-02 13:44:07 -0400

    Simplified skein script

--- skein-ec2.py
+++ skein-ec2.py
@@ -18,8 +18,6 @@ import boto
 ec2_instance_type = 'c1.xlarge' # our AMI uses 8 cores
 num_instances = 20
 ami_id = 'ami-6865f158' # this is our custom AMI
-key = 'xkcd-crackers' #this is your SSH keypair
-sec = ['xkcd-crackers'] # this is your security group
 
 
 conn = boto.connect_ec2()
@@ -27,10 +25,7 @@ regions = boto.ec2.regions()
 usw2 = regions[2].connect()
 
 def launch_instance():
-  return usw2.run_instances(ami_id, 
-                            instance_type=ec2_instance_type,
-                            security_groups=sec,
-                            key_name=key)
+  return usw2.run_instances(ami_id, instance_type=ec2_instance_type)
 
 def launch_instances(n):
   return [launch_instance() for i in range(0,n)]
